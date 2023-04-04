d713622ae7053e85ec07753745281e51a327189b
Alexander <yusovsky2@gmail.com>
2023-04-04 12:19:09 +0300

    button block done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,28 @@
 import { Component } from 'react';
 import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 
 export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  // работает именно стрелка. почему?
+  handleButtonClick = e => {
+    const { name } = e.currentTarget;
+
+    // обновить значение относительно предыдущих - через функцию
+    // объясняется асинхронностью
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        [name]: prevState[name] + 1,
+      };
+    });
+  };
+
   render() {
     return (
       <div
@@ -15,7 +36,10 @@ export class App extends Component {
         }}
       >
         <Section title="React homework template">
-          React homework template
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.handleButtonClick}
+          />
         </Section>
       </div>
     );
